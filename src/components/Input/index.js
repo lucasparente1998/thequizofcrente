70f6ac0c243cd76f0af24b71e56f5c9d524f4fad
Lucas Parente Goncalves <lucasparente@id.uff.br>
2021-01-31 15:23:27 -0300

    ajeitando projeto e finalizando

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 const InputBase = styled.input`
   width: 100%;
@@ -12,6 +12,11 @@ const InputBase = styled.input`
   border-radius: ${({ theme }) => theme.borderRadius};
   outline: 0;
   margin-bottom: 25px;
+  ::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+    color: ${({ theme }) => theme.colors.contrastText}DD;
+    opacity: 1; /* Firefox */
+  }
+
 `;
 
 export default function Input({ onChange, placeholder, ...props }) {
